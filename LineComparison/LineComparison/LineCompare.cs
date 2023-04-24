9485507cd9d22eea99e83dc5113cd6d184d34553
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-24 18:47:30 +0530

    Check equality of lines

--- LineComparison/LineComparison/LineCompare.cs
+++ LineComparison/LineComparison/LineCompare.cs
@@ -18,10 +18,11 @@ namespace LineComparison
             this.y_Two = y_Two;
         }
         //Method
-        public void Calculate()
+        public double Calculate()
         {
             double length = Math.Sqrt(Math.Pow((x_Two - x_One), 2) + Math.Pow((y_Two - y_One), 2));
             Console.WriteLine("Length of line is: " + length);
+            return length;
         }
     }
 }
--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -4,8 +4,18 @@
     {
         public static void Main(string[] args)
         {
-            LineCompare obj = new LineCompare(2, 3, 4, 5);
-            obj.Calculate();
+            LineCompare obj_One = new LineCompare(2, 3, 4, 5);
+            double length_One = obj_One.Calculate();
+            LineCompare obj_Two = new LineCompare(2, 3, 4, 5);
+            double length_Two = obj_Two.Calculate();
+            if (length_One.Equals(length_Two))
+            {
+                Console.WriteLine("Length of both lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Length of both lines are not equal");
+            }
             Console.ReadKey();
         }
     }
